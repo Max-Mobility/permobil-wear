7cf0dc25ea2f33ffeb8141f1bc3cb4c8adf4721d
Brad Martin <bradwaynemartin@gmail.com>
2019-01-21 11:34:10 -0600

    implement sentry

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,14 @@
-﻿import './async-await';
+﻿import './core/async-await';
 import * as application from 'tns-core-modules/application';
 import { Kinvey } from 'kinvey-nativescript-sdk';
+import { Sentry } from 'nativescript-sentry';
+import { APP_KEY, APP_SECRET } from './core/kinvey-keys';
 
-Kinvey.init({
-  appKey: 'kid_SyIIDJjdM',
-  appSecret: '3cfe36e6ac8f4d80b04014cc980a4d47'
-});
+Kinvey.init({ appKey: `${APP_KEY}`, appSecret: `${APP_SECRET}` });
+
+// init sentry - DNS key is in the SmartEvalKinvey package
+Sentry.init(
+  'https://234acf21357a45c897c3708fcab7135d:bb45d8ca410c4c2ba2cf1b54ddf8ee3e@sentry.io/1376181'
+);
 
 application.run({ moduleName: 'app-root' });
