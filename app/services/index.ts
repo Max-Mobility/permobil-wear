d9ca404a3cdc87ef2bc925b0ed369b8f5b25e69d
Brad Martin <bradwaynemartin@gmail.com>
2019-01-21 12:14:05 -0600

    export services for DI

--- app/services/index.ts
+++ app/services/index.ts
@@ -1,1 +1,7 @@
+import { BluetoothService } from './bluetooth.service';
+
+// exporting an array of any service that will be used for dependency injection on app.ts during start up
+export const SERVICES = [BluetoothService];
+
+// export all services so they're able to be imported for types when used
 export * from './bluetooth.service';
