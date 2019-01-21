2aa84710ae2707e2efb5ee778d47ae8cd599d82f
Brad Martin <bradwaynemartin@gmail.com>
2019-01-21 11:34:26 -0600

    use core

---
+++ app/core/kinvey-keys.ts
@@ -0,0 +1,10 @@
+import { SmartEvalKeys } from 'smart-eval-kinvey';
+
+// use these for dev environment
+// export const APP_KEY = SmartEvalKeys.DEV_KEY;
+// export const APP_SECRET = SmartEvalKeys.DEV_SECRET;
+
+// use these for production environment
+export const APP_KEY = SmartEvalKeys.PROD_KEY;
+export const APP_SECRET = SmartEvalKeys.PROD_SECRET;
+export const HOST_URL = SmartEvalKeys.HOST_URL;
