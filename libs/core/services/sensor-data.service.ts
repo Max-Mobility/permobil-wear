d14b971bf9095b9b76eae285520cd3cb42cbf3f8
Brad Martin <bradwaynemartin@gmail.com>
2019-04-05 17:26:47 -0500

    get UUID ourselves

--- libs/core/services/sensor-data.service.ts
+++ libs/core/services/sensor-data.service.ts
@@ -5,6 +5,7 @@ import {
   AccelerometerData,
   getSensorList
 } from 'nativescript-accelerometer-advanced';
+import * as app from 'tns-core-modules/application';
 import { Log } from '../utils';
 
 @Injectable()
@@ -16,44 +17,49 @@ export class SensorDataService {
   }
 
   saveRecord(sensor_data: AccelerometerData[]) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const deviceSensors = getSensorList() as android.hardware.Sensor[];
-        const sensor_list = [];
-        deviceSensors.forEach(i => {
-          sensor_list.push({
-            id: i.getId(),
-            vendor: i.getVendor(),
-            version: i.getVersion(),
-            name: i.getName(),
-            power: i.getPower(),
-            type: i.getType(),
-            string_type: i.getStringType(),
-            max_range: i.getMaximumRange(),
-            max_delay: i.getMaxDelay(),
-            min_delay: i.getMinDelay(),
-            resolution: i.getResolution(),
-            reporting_mode: i.getReportingMode()
-          });
+    const deviceSensors = getSensorList() as android.hardware.Sensor[];
+    const sensor_list = [];
+
+    deviceSensors.forEach(i => {
+      if (i) {
+        sensor_list.push({
+          id: i.getId(),
+          vendor: i.getVendor(),
+          version: i.getVersion(),
+          name: i.getName(),
+          power: i.getPower(),
+          type: i.getType(),
+          string_type: i.getStringType(),
+          max_range: i.getMaximumRange(),
+          max_delay: i.getMaxDelay(),
+          min_delay: i.getMinDelay(),
+          resolution: i.getResolution(),
+          reporting_mode: i.getReportingMode()
         });
-        Log.D(`Saving data collection record for WatchData to Kinvey...`);
-        const dbRecord = {
-          sensor_data,
-          sensor_list,
-          device_uuid: device.uuid,
-          device_manufacturer: device.manufacturer,
-          device_model: device.model,
-          device_os_version: device.osVersion,
-          device_sdk_version: device.sdkVersion
-        };
-        await this._datastore.save(dbRecord);
-        Log.D('WatchData record saved on Kinvey.');
-        resolve();
-      } catch (error) {
-        Log.E('Error saving sensor data to Kinvey.', error);
-        reject(error);
       }
     });
+
+    Log.D(`Saving data collection record for WatchData to Kinvey...`);
+    const nApp = app.android.nativeApp as android.app.Application;
+    const resolver = nApp.getContentResolver();
+    const deviceUUID = android.provider.Settings.Secure.getString(
+      resolver,
+      android.provider.Settings.Secure.ANDROID_ID
+    );
+
+    const dbRecord = {
+      sensor_data,
+      sensor_list,
+      device_uuid: deviceUUID,
+      // device_uuid: device.uuid,
+      device_manufacturer: device.manufacturer,
+      device_model: device.model,
+      device_os_version: device.osVersion,
+      device_sdk_version: device.sdkVersion
+    };
+
+    // now save to kinvey data collection
+    return this._datastore.save(dbRecord);
   }
 }
 
