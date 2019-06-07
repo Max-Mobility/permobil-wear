f2d275ad7e02e9bc9744979db2cf16689b72993d
William Emfinger <waemfinger@gmail.com>
2019-06-07 17:07:05 -0500

    WIP #119 added forked nativescript-vibrate plugin which updates to the new VibrationEffect api.

---
+++ forked-plugins/nativescript-vibrate/vibrate.ios.ts
@@ -0,0 +1,16 @@
+import { Common } from './vibrate.common';
+
+export class Vibrate extends Common {
+  hasVibrator(): boolean {
+    return true;
+  }
+
+  vibrate(time: number | number[], repeat: number) {
+    // a bit of a magic number, but this seems to be the most reliable way to vibrate an iOS device
+    AudioServicesPlaySystemSoundWithCompletion(1352, null);
+  }
+
+  cancel() {
+    return;
+  }
+}
