ccdbaee0c72e08e1a75171451768112fe660e3ac
William Emfinger <waemfinger@gmail.com>
2019-06-13 16:36:57 -0500

    update power assist colors to come from permobil pantone catalogue.

--- apps/smartdrive-app/app/namespaces/power-assist.ts
+++ apps/smartdrive-app/app/namespaces/power-assist.ts
@@ -3,9 +3,9 @@ import { Color } from 'tns-core-modules/color';
 export namespace PowerAssist {
   export const InactiveRingColor = new Color('#000000');
   export const ActiveRingColor = new Color('#006ea4');
-  export const TrainingRingColor = new Color('#2fa52f');
+  export const TrainingRingColor = new Color('#00a73c');
   export const ConnectedRingColor = new Color('#006ea4');
-  export const DisconnectedRingColor = new Color('#a52f2f');
+  export const DisconnectedRingColor = new Color('#db4537');
 
   export enum State {
     Inactive,
